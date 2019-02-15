33e26e53d9e53cb34180ccbd166d7e16fc30dd83
Philip Xu <xianxu@chicagobooth.edu>
2019-02-15 13:18:31 -0600

    Fourth Commit

--- publish.sh
+++ publish.sh
@@ -3,3 +3,20 @@
 # Generate the Slides and Pages
 jupyter-nbconvert Notebooks/Index.ipynb --reveal-prefix=reveal.js
 mv Notebooks/Index.html index.html
+
+cd Notebooks
+arr=(*.ipynb)
+cd ..
+for f in "${arr[@]}"; do
+
+   # Chop off the extension
+   filename=$(basename "$f")
+   extension="${filename##*.}"
+   filename="${filename%.*}"
+
+   # Convert the Notebook to HTML
+   jupyter-nbconvert --to html Notebooks/"$filename".ipynb
+   # Move to the Html directory
+   mv Notebooks/"$filename".html  Html/"$filename".html
+
+done
