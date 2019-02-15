26d54d70db9a3b6a50e20b0d1d07c4caea665f1e
Philip Xu <xianxu@chicagobooth.edu>
2019-02-15 13:27:31 -0600

    Update Slides

--- publish.sh
+++ publish.sh
@@ -20,3 +20,16 @@ for f in "${arr[@]}"; do
    mv Notebooks/"$filename".html  Html/"$filename".html
 
 done
+
+# Push the updates to gh-pages
+mkdir -p /tmp/workspace
+cp -r * /tmp/workspace/
+git add -A .
+git commit -m "Update Slides"
+git checkout -B gh-pages
+cp -r /tmp/workspace/* .
+git add -A .
+git commit -m "Update Slides"
+git push origin master gh-pages
+git checkout master
+rm -rf /tmp/workspace
